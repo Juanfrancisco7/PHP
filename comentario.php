bee559379e6516056ac5bf977afad2a6d4eef59b
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-04-30 12:48:36 +0200

    se agregan comentarios

--- comentario.php
+++ comentario.php
@@ -1,3 +1,12 @@
 <?php
 // mi primer hola mundo en COMENTARIOS
-echo "Hola mundo hola";
+echo "Hola mundo 1";
+
+# hola mundo hola mundo
+echo "hola mundo2";
+
+/*
+el autor de este codigo es Juan Francisco
+2024
+*/
+echo "hola mundo 3";
