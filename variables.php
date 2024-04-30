4bad9d4a72c16db65b69e1b2d2d7f66e1ffd4c8e
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-04-30 14:16:36 +0200

    se agregan variables predefinidias

--- variables.php
+++ variables.php
@@ -2,3 +2,23 @@
 
 $NombreCompleto="Juan Francisco";
 echo  $NombreCompleto;
+
+/*
+Variables predefinidas
+1. $GLOBALS - Hace referencia a todas las variables disponibles en el ámbito global
+2.$_SERVER -- Información del entorno del servidor y de ejecución
+3. $_GET - Variables HTTP GET
+4. _POST - Variables POST de HTTP
+5. $_FILES - Variables de subida de ficheros HTTP
+6. $_REQUEST - Variables HTTP Request
+7. $_SESSION - Variables de sesión
+8. $_ENV - Variables de entorno
+6
+$_COOKIE - Cookies HTTP
+10. $php_errormsg -- El mensaje de error anterior
+11. $HTTP_RAW_POST_DATA - Datos POST sin tratar
+12. $http_response_header - Encabezados de respuesta HTTP
+13. $argc - El número de argumentos pasados a un script
+14. $argv - Array de argumentos pasados a un script
+15. $this - Palabra reservada utilizada en programación orientada a objetos 
+/*
