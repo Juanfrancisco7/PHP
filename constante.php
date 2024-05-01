3108006051fbc094bb18d2b04982c1886fb74c6f
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-01 10:57:23 +0200

    actualizacion de comentarios

--- constante.php
+++ constante.php
@@ -1,7 +1,7 @@
 <?php
 // se puede definir una constante de esta forma
 const NOMBRE1="Ashley";
-
+# esta es la que se usa en la actualidad
 echo NOMBRE1;
 // o tambien se puede definir de esta forma 
 define ("NOMBRE2", "Juan Francisco");
