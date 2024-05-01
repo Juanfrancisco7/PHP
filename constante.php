9189b3ecc7b0bd5862a193714dcfa316dbcbc48f
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-01 10:46:59 +0200

    se agregan comentarios en carpeta constante

--- constante.php
+++ constante.php
@@ -1,9 +1,9 @@
 <?php
-
+// se puede definir una constante de esta forma
 const NOMBRE1="Ashley";
 
 echo NOMBRE1;
-
+// o tambien se puede definir de esta forma 
 define ("NOMBRE2", "Juan Francisco");
 
 echo NOMBRE2;
