dc617dac2ceb005cd02db6c2fdca85861c8c8c6a
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-01 11:57:59 +0200

    explico el array y modifico

--- arrays.php
+++ arrays.php
@@ -1,4 +1,6 @@
 <?php
 $estudiantes=array("carlos", "Juan", "pepe");
 // aqui seleccionamos a pepe colocando el 2
+$estudiantes[2]="daniel";
+// asi modifico los valores del array
 echo $estudiantes [2];
