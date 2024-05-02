bd725b8db5ba195158a684f3bdfb9d1eeb206ab6
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-02 18:01:50 +0200

    se agrega ejemplo con if elseif

--- estructura_condicional_multiple.php
+++ estructura_condicional_multiple.php
@@ -23,3 +23,23 @@ switch ($dia) {
     default:
         echo "Hoy no es un día de la semana válido.";
 }
+
+//tambien con if-ifelse
+
+// Definimos una variable que representa el día de la semana
+$dia = "lunes";
+
+// Evaluamos la variable $dia usando if y elseif para determinar el mensaje a imprimir
+if ($dia == "lunes") {
+    echo "Hoy es lunes. Es el primer día laboral de la semana.";
+} elseif ($dia == "martes") {
+    echo "Hoy es martes. A mitad de camino de la semana.";
+} elseif ($dia == "miércoles") {
+    echo "Hoy es miércoles. Se acerca el fin de semana.";
+} elseif ($dia == "jueves") {
+    echo "Hoy es jueves. Mañana es viernes.";
+} elseif ($dia == "viernes") {
+    echo "Hoy es viernes. ¡Fin de semana cerca!";
+} else {
+    echo "Hoy no es un día de la semana válido.";
+}
