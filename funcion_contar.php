e98b7dc68f4604c66e33ecc5b66b7bde6d6348c2
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-07 16:56:35 +0200

    se agrega carpeta para contar caracteres

---
+++ funcion_contar.php
@@ -0,0 +1,8 @@
+<?php
+//para contar caracteres o cuantas palabras tiene un string
+
+$cadena_Texto="Hola Mundo";
+
+$longitud=strlen($cadena_Texto);
+// se usa srtlen para contar cada alemento incluido espacios
+echo $longitud;
--- funciones_string.php
+++ funciones_string.php
@@ -3,3 +3,8 @@
 $cadena_texto="Hola Mundo";
 
 echo strtolower($cadena_texto);
+
+//strtolower sirve para todo minuscula
+//str toupper para mayuscula
+//ucfirt primera letra en mayuscula
+//ucwords cada letra en de inciial de palabra en maysucula
