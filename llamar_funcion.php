86ead5df67bc60f3ccb927838d9afcd975605e6f
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-07 11:11:07 +0200

    se agrega nueva carpeta y se llama la funcion de otra carpeta

---
+++ llamar_funcion.php
@@ -0,0 +1,15 @@
+<?php
+
+include "funciones.php";
+
+echo "El promedio es: " . promedio_alumno(7, 9, 6);
+echo "<br>";
+echo "El promedio es: " . promedio_alumno(8, 1, 6);
+echo "<br>";
+echo "El promedio es: " . promedio_alumno(7, 4, 6);
+echo "<br>";
+echo "El promedio es: " . promedio_alumno(2, 9, 6);
+echo "<br>";
+echo "El promedio es: " . promedio_alumno(7, 9, 3);
+
+// llame la funcion de una carpeta a otra con include
