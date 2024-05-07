cb57b009b15d43c5852ce8a468c1d6b02e3550fb
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-07 09:23:41 +0200

    se actualiza ejemplo

--- include.php
+++ include.php
@@ -2,3 +2,7 @@
 
 //asi se agrega un archvio con include para reutilizar el codigo de php a php
 include("for.php");
+// si se coloca 2 veces, aparecera dos veces y asi sucesivamente
+include("for.php");
+include("for.php");
+include("for.php");
