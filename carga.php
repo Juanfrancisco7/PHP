c16b741bef4c6322cfb636d8731e7490ef7d5a0b
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-08 16:10:00 +0200

    se agrega codigo php para interactuar con envio de archivo en programa

--- carga.php
+++ carga.php
@@ -1,7 +1,27 @@
 <?php
-echo $_FILES['fichero']['name']. "<br>";
-echo $_FILES['fichero']['tmp_name']. "<br>";
-echo $_FILES['fichero']['type']. "<br>";
-echo $_FILES['fichero']['error']. "<br>";
-echo $_FILES['fichero']['size']. "<br>";
+
+// Verificar si el directorio "archivos" no existe
+if (!is_dir("archivos")) {
+    // Intentar crear el directorio "archivos"
+    if (!mkdir("archivos", 0777)) {
+        echo "Error al crear el directorio";
+        exit(); // Salir del script si no se pudo crear el directorio
+    }
+    // Asignar permisos al directorio "archivos"
+    chmod("archivos", 0777);
+}
+
+// Verificar si se ha enviado un archivo
+if (isset($_FILES["fichero"]) && $_FILES["fichero"]["error"] === UPLOAD_ERR_OK) {
+    // Mover el archivo cargado al directorio "archivos"
+    $nombre_archivo = $_FILES["fichero"]["name"];
+    $ruta_destino = "archivos/" . $nombre_archivo;
+    if (move_uploaded_file($_FILES["fichero"]["tmp_name"], $ruta_destino)) {
+        echo "El archivo $nombre_archivo se ha subido correctamente.";
+    } else {
+        echo "Error al subir el archivo $nombre_archivo.";
+    }
+} else {
+    echo "No se ha seleccionado ningún archivo o ha ocurrido un error durante la carga.";
+}
 ?>
