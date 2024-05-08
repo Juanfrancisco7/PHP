d6952ae9224db9a180c75ec550987bb6da3c9e81
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-05-08 15:36:38 +0200

    se agrega carga de archvi mediante servidor

--- isset.php
+++ isset.php
@@ -7,3 +7,18 @@ if (is_null($numero)){
 } else {
     echo "No es nula";
 }
+
+/*<?php
+// Supongamos que recibimos los datos del formulario
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+
+// Verificar si el campo de teléfono está definido y no es nulo
+if (!is_null($telefono)) {
+    echo "El usuario proporcionó un número de teléfono: $telefono";
+} else {
+    echo "El usuario no proporcionó un número de teléfono.";
+}
+?>
+*/
